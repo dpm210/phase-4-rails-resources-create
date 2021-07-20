5322dbcbc98d4a8fb18c4545eed3adeac0027fd7
Daniel Mueller <danielpetermueller@gmail.com>
2021-07-20 10:08:00 -0400

    commit tree 164bd95e746c696710f8fd9478b9dbf1013f46cf

--- db/migrate/20210502132944_create_birds.rb
+++ db/migrate/20210502132944_create_birds.rb
@@ -1,10 +1,8 @@
 class CreateBirds < ActiveRecord::Migration[6.1]
-  def change
-    create_table :birds do |t|
-      t.string :name
-      t.string :species
-
-      t.timestamps
-    end
-  end
+  create_table "birds", force: :cascade do |t|
+    t.string "name"
+    t.string "species"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end 
 end
